4b1513cc66353643e87415cf2dca3579ae167acb
Farhan <farhan.labib@tblbd.com>
2026-04-29 15:38:46 +0600

    new company logo

--- resources/views/components/home/our-clients.blade.php
+++ resources/views/components/home/our-clients.blade.php
@@ -1,54 +1,65 @@
 <section>
-    <div
-        class="relative flex flex-col mx-3 md:mx-20 my-20 bg-white drop-shadow-xl border-gray-200 dark:bg-gradient-to-r dark:from-slate-900 dark:to-slate-700 rounded-lg">
-        <div class="w-full">
-            <div class="container flex flex-col items-center gap-8 mx-auto my-20 px-5">
-                <h2 class="border-bottom-on-hover-dark text-slate-900 dark:text-gray-300 text-2xl sm:text-4xl sm:leading-snug tracking-tight font-bold text-left cursor-pointer">
-                    Some of our Valuable Clients
-                </h2>
-                <div
-                    class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/caab-logo.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/BSMMU.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/AMG.svg") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/aci.png") }}" width="250" height="200">
-                </div>
-                <div
-                    class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/sigma.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/posco.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/HDC.svg") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/BCIC.png") }}" width="250" height="200">
-                </div>
-                <div
-                    class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/ZHAOFENG.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/ACORN.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/BFDC.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/IHI.png") }}" width="250" height="200">
-                </div>
-                <div
-                    class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/SMCC.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/BAF.png") }}" width="250" height="200">
-                    <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer" alt=""
-                         src="{{ asset("/images/company-logos/zilla-porishod.png") }}" width="250" height="200">
-                </div>
-            </div>
-        </div>
-    </div>
+     <div
+          class="relative flex flex-col mx-3 md:mx-20 my-20 bg-white drop-shadow-xl border-gray-200 dark:bg-gradient-to-r dark:from-slate-900 dark:to-slate-700 rounded-lg">
+          <div class="w-full">
+               <div class="container flex flex-col items-center gap-8 mx-auto my-20 px-5">
+                    <h2
+                         class="border-bottom-on-hover-dark text-slate-900 dark:text-gray-300 text-2xl sm:text-4xl sm:leading-snug tracking-tight font-bold text-left cursor-pointer">
+                         Some of our Valuable Clients
+                    </h2>
+                    <div
+                         class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/caab-logo.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/BSMMU.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/AMG.svg") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/aci.png") }}" width="250" height="200">
+                    </div>
+                    <div
+                         class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/sigma.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/posco.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/HDC.svg") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/BCIC.png") }}" width="250" height="200">
+                    </div>
+                    <div
+                         class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/ZHAOFENG.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/ACORN.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/BFDC.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/IHI.png") }}" width="250" height="200">
+                    </div>
+                    <div
+                         class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/SMCC.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/BAF.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/zilla-porishod.png") }}" width="250"
+                              height="200">
+                    </div>
+                    <div
+                         class="flex flex-wrap items-center justify-center w-full gap-6 lg:gap-0 lg:flex-nowrap lg:justify-between">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/serengeti.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/ansart.png") }}" width="250" height="200">
+                         <img class="transition-all filter grayscale hover:grayscale-0 duration-300 cursor-pointer"
+                              alt="" src="{{ asset("/images/company-logos/ccecc.png") }}" width="250" height="200">
+                    </div>
+               </div>
+          </div>
+     </div>
 </section>
